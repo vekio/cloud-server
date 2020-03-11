eaba924b5b0d87c88533ee3472106d9f97a0bf47
vekio <albertocasta@protonmail.com>
2020-03-11 23:22:21 +0100

    Add nextcloud record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -10,6 +10,7 @@ resource "digitalocean_record" "www_domain_tld" {
   type    = "CNAME"
   name    = "www"
   value   = format("%s.", var.domain_tld)
+  ttl     = 43200
 }
 
 # traefik.domain.tld
@@ -18,4 +19,14 @@ resource "digitalocean_record" "traefik_domain_tld" {
   type    = "CNAME"
   name    = "traefik"
   value   = format("%s.", var.domain_tld)
+  ttl     = 43200
+}
+
+# nextcloud.domain.tld
+resource "digitalocean_record" "nextcloud_domain_tld" {
+  domain  = digitalocean_domain.domain_tld.name
+  type    = "CNAME"
+  name    = "nextcloud"
+  value   = format("%s.", var.domain_tld)
+  ttl     = 43200
 }
